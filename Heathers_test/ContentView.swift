424370e5c02192fb1254e1b1a38a06d653ad75e7
AGCTanaka <bluejb.bluejay@iloud.com>
2019-10-25 11:39:10 +0900

    Initial Commit

---
+++ Heathers_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Heathers_test
+//
+//  Created by 田中千夏 on 2019/10/25.
+//  Copyright © 2019 田中千夏. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
